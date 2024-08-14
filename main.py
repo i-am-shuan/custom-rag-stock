8a5a779fb99d2931772ca1753aeaaf2d4bd65467
South Korea Shuan <161431602+i-am-shuan@users.noreply.github.com>
2024-08-14 13:42:42 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import sys
+sys.path.append("./1_stock_qna")
+sys.path.append("./2_stock_query")
+sys.path.append("./3_stock_tools")
+sys.path.append("./4_stock_analysis")
+from stock_qna_app import stock_qna 
+from stock_query_app import stock_query
+from stock_tools_app import stock_tools
+from stock_analysis_app import stock_analysis
+from PIL import Image
+
+
+
+#st.sidebar.success("Select a a tool below.")
+
+#image = Image.open('Artificial-Intelligence-Stocks.jpg')
+image = Image.open('images/kb_friends.png')
+st.image(image, caption='')
+
+
+page_names_to_funcs = {
+    "Stock Analysis": stock_analysis,
+    "Stock Tools": stock_tools,
+    "Stock Q&A": stock_qna, 
+    "Stock Query": stock_query,
+}
+
+#demo_name = st.sidebar.selectbox("Choose a demo", page_names_to_funcs.keys())
+demo_name = "Stock Analysis"
+page_names_to_funcs[demo_name]()
